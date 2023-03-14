6ea3bb15458b34bc0db09f3ba856248a9ceb94f6
priyanshu0085984 <priyanshu0553.be20@chitkara.edu.in>
2023-03-14 17:43:14 +0530

    Setting up app & server files

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'});
+
+const DB = process.env.DATABASE.replace("<PASSWORD>",DATABASE_PASSWORD);
+
+mongoose.connect(DB)
+.then(()=>{
+    console.log('DB connection successful');
+})
+.catch((err)=>{
+    console.log(`Error connecting Database. Error: ${err}`);
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`App listening to port: ${PORT}`);
+});
